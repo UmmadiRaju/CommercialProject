2a2095f1dd48c16182f4a1d0bb6eff6686a10cd4
PriyankaMunukoti <101922985+PriyankaMunukoti@users.noreply.github.com>
2025-05-19 17:50:05 +0530

    Logincomments

--- src/test/java/testExecutionSuite/Login.java
+++ src/test/java/testExecutionSuite/Login.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class Login extends ReusableCode {
 	public void logintest() throws InterruptedException, IOException
 	{
-		xp("xp").click(); // Add comments
-		xp("xe").sendKeys(obj.getProperty("email"));
-		xp("xpd").sendKeys(obj.getProperty("password"));
-		xp("xl").click();
+		xp("xp").click(); // Login Hyperlink
+		xp("xe").sendKeys(obj.getProperty("email")); // Email Field
+		xp("xpd").sendKeys(obj.getProperty("password")); // Password Field
+		xp("xl").click(); // Login Button
 		timeout(2);
 		screenshot("login");
 	}
