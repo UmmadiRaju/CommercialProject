27e3f5d8bddffbe107532fad956e0ae7042702ed
Vasantha207 <ummidivasantha8@gmail.com>
2025-05-26 00:07:13 +0530

    Subscript

--- src/test/java/testExecutionSuite/TestExecute.java
+++ src/test/java/testExecutionSuite/TestExecute.java
@@ -1,5 +1,6 @@
 package testExecutionSuite;
 
+import java.awt.AWTException;
 import java.io.IOException;
 
 import org.testng.annotations.Test;
@@ -8,6 +9,10 @@ import basePack.BaseClass;
 public class TestExecute extends BaseClass {
 	Register r;
 	Login l;
+	Contact_us c;
+	Subscription_homepage sh; 
+	Subscription_cartpage sc;
+	
 	@Test(priority=1)
 	public void Register() throws IOException, InterruptedException {
 	 r = new Register();
@@ -32,4 +37,22 @@ public class TestExecute extends BaseClass {
 		l = new Login();
 		l.logintest();
 	}
+	@Test(priority=6)
+	public void Contact_us() throws InterruptedException, IOException, AWTException {
+		c = new Contact_us();
+		c.contact();
+	}
+	
+	@Test(priority=10)
+	public void Subscription_homepage() throws InterruptedException, IOException, AWTException {
+		sh = new Subscription_homepage();
+		sh.subscription_homepage();
+	}
+	
+	@Test(priority=11)
+	public void Subscription_cartpage() throws InterruptedException, IOException, AWTException {
+		sc = new Subscription_cartpage();
+		sc.subscription_cartpage();
+	}
+	
 }
